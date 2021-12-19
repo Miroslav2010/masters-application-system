6a7d0fbf95d5e00967c25dbc4a76d7fc36649feb
Miroslav <radicmiroslav101@gmail.com>
2021-12-19 19:51:21 +0100

    initial commit

---
+++ src/main/java/mk/ukim/finki/masterapplicationsystem/MasterApplicationSystemApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.masterapplicationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterApplicationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterApplicationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/masterapplicationsystem/MasterApplicationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.masterapplicationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterApplicationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
