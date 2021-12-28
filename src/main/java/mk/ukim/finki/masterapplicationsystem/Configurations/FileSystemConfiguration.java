95617079d8271e83b4a1d5423ec7049b79b527da
Jungelr <viktor.panovski@yahoo.com>
2021-12-28 14:19:31 +0100

    DocumentService

---
+++ src/main/java/mk/ukim/finki/masterapplicationsystem/Configurations/FileSystemConfiguration.java
@@ -0,0 +1,27 @@
+package mk.ukim.finki.masterapplicationsystem.Configurations;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "filesystem")
+public class FileSystemConfiguration {
+    private boolean removeData=false;
+    private String dataDirectory=".";
+
+    public boolean isRemoveData() {
+        return removeData;
+    }
+
+    public void setRemoveData(boolean removeData) {
+        this.removeData = removeData;
+    }
+
+    public String getDataDirectory() {
+        return dataDirectory;
+    }
+
+    public void setDataDirectory(String dataDirectory) {
+        this.dataDirectory = dataDirectory;
+    }
+}
---
+++ src/main/java/mk/ukim/finki/masterapplicationsystem/service/DocumentService.java
@@ -0,0 +1,7 @@
+package mk.ukim.finki.masterapplicationsystem.service;
+
+import mk.ukim.finki.masterapplicationsystem.domain.Document;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface DocumentService {
+}
