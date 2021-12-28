fb31fd4c6d4adf2ba764e5fe0780eae526bf9140
Hristijan <hristijan.peshov@gmail.com>
2021-12-28 19:05:57 +0100

    Reformat files

--- src/main/java/mk/ukim/finki/masterapplicationsystem/configurations/FileSystemConfiguration.java
+++ src/main/java/mk/ukim/finki/masterapplicationsystem/configurations/FileSystemConfiguration.java
@@ -1,4 +1,4 @@
-package mk.ukim.finki.masterapplicationsystem.Configurations;
+package mk.ukim.finki.masterapplicationsystem.configurations;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties(prefix = "filesystem")
 public class FileSystemConfiguration {
-    private boolean removeData=false;
-    private String dataDirectory=".";
+    private boolean removeData = false;
+    private String dataDirectory = ".";
 
     public boolean isRemoveData() {
         return removeData;
--- src/main/java/mk/ukim/finki/masterapplicationsystem/repository/implementation/FileRepositoryImplementation.java
+++ src/main/java/mk/ukim/finki/masterapplicationsystem/repository/implementation/FileRepositoryImplementation.java
@@ -1,12 +1,9 @@
 package mk.ukim.finki.masterapplicationsystem.repository.implementation;
 
-import mk.ukim.finki.masterapplicationsystem.Configurations.FileSystemConfiguration;
+import mk.ukim.finki.masterapplicationsystem.configurations.FileSystemConfiguration;
 import mk.ukim.finki.masterapplicationsystem.repository.FileRepository;
 import org.aspectj.util.FileUtil;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.FileSystemResourceLoader;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,7 +20,7 @@ import java.util.UUID;
 @Repository
 public class FileRepositoryImplementation implements FileRepository {
     private FileSystemConfiguration fileSystemConfiguration;
-    private static String dataFolderName="/MasterDocuments";
+    private static String dataFolderName = "/MasterDocuments";
     private static String applicationDocumentsFolderName = "/documents-application";
     private static String draftsFolderName = "/drafts";
     private static String reportsFolderName = "/reports";
@@ -31,22 +28,22 @@ public class FileRepositoryImplementation implements FileRepository {
     public FileRepositoryImplementation(FileSystemConfiguration fileSystemConfiguration) {
         this.fileSystemConfiguration = fileSystemConfiguration;
     }
+
     @PostConstruct
     public void init() throws FileSystemException {
         File folder = new File(fileSystemConfiguration.getDataDirectory() + dataFolderName);
-        if(fileSystemConfiguration.isRemoveData()){
+        if (fileSystemConfiguration.isRemoveData()) {
             FileUtil.deleteContents(folder);
         }
         boolean result = folder.mkdirs();
-        if(!result)
-        {
+        if (!result) {
             throw new FileSystemException("Could not create folders!");
         }
     }
 
-    public String save(UUID userId, MultipartFile file,String subFolder) throws IOException {
+    public String save(UUID userId, MultipartFile file, String subFolder) throws IOException {
         this.checkDirectoriesForUser(userId);
-        String saveLocation = fileSystemConfiguration.getDataDirectory() + "/" + userId.toString()+subFolder+"/"+new Date().getTime()+file.getName();
+        String saveLocation = fileSystemConfiguration.getDataDirectory() + "/" + userId.toString() + subFolder + "/" + new Date().getTime() + file.getName();
         File localFile = new File(saveLocation);
         file.transferTo(localFile);
         return saveLocation;
@@ -55,7 +52,7 @@ public class FileRepositoryImplementation implements FileRepository {
     @Override
     public void delete(String location) {
         File localFile = new File(location);
-        if (localFile.exists()){
+        if (localFile.exists()) {
             localFile.delete();
         }
     }
@@ -63,30 +60,31 @@ public class FileRepositoryImplementation implements FileRepository {
     @Override
     public Resource findByLocation(String location) throws FileNotFoundException {
         File file = new File(location);
-        if(!file.exists() || file.isDirectory()){
+        if (!file.exists() || file.isDirectory()) {
             throw new FileNotFoundException();
         }
         return new FileSystemResource(file);
     }
 
 
-    private void checkDirectoriesForUser(UUID userId){
+    private void checkDirectoriesForUser(UUID userId) {
         boolean isDirectoryPresent = this.isDirectoryStructureCreated(userId);
-        if (!isDirectoryPresent){
+        if (!isDirectoryPresent) {
             this.createDirectoryStructureForUser(userId);
         }
     }
 
-    private boolean isDirectoryStructureCreated(UUID userId){
-        String userFolder=fileSystemConfiguration.getDataDirectory()+dataFolderName+"/"+userId.toString();
+    private boolean isDirectoryStructureCreated(UUID userId) {
+        String userFolder = fileSystemConfiguration.getDataDirectory() + dataFolderName + "/" + userId.toString();
         File folder = new File(userFolder);
         return folder.exists();
     }
-    private void createDirectoryStructureForUser(UUID userId){
-        String userFolder=fileSystemConfiguration.getDataDirectory()+dataFolderName+"/"+userId.toString();
-        File documentsApplicationFolder= new File(userFolder+applicationDocumentsFolderName);
-        File draftsFolder = new File(userFolder+draftsFolderName);
-        File reportsFolder = new File(userFolder+reportsFolderName);
+
+    private void createDirectoryStructureForUser(UUID userId) {
+        String userFolder = fileSystemConfiguration.getDataDirectory() + dataFolderName + "/" + userId.toString();
+        File documentsApplicationFolder = new File(userFolder + applicationDocumentsFolderName);
+        File draftsFolder = new File(userFolder + draftsFolderName);
+        File reportsFolder = new File(userFolder + reportsFolderName);
         documentsApplicationFolder.mkdirs();
         draftsFolder.mkdirs();
         reportsFolder.mkdirs();
