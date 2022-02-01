26f023e7c90aabf6fe0ae56a8ff5ce882a631f8c
Hristijan <hristijan.peshov@gmail.com>
2022-02-01 22:30:07 +0100

    Validate Page addition

---
+++ src/main/java/mk/ukim/finki/masterapplicationsystem/domain/dto/response/DocumentDTO.java
@@ -0,0 +1,16 @@
+package mk.ukim.finki.masterapplicationsystem.domain.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Getter
+@AllArgsConstructor
+public class DocumentDTO {
+
+    private String name;
+
+    private String location;
+
+}
